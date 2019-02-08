584c4d1371dd080241dfc2eecac3a9c240a0ee45
Hancel PV <34185789+hancelpv@users.noreply.github.com>
2019-02-09 00:04:46 +0530

    adding main

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -1,17 +1,20 @@
 
-series = [0, 1]
+def main()
+    series = [0, 1]
 
-# Taking User input 
-num_points = int(input("Enter num of terms required in Fibonacci Sequence :"))
+    # Taking User input 
+    num_points = int(input("Enter num of terms required in Fibonacci Sequence :"))
 
-for i in range(2, num_points+1):
-#     print(i)
+    for i in range(2, num_points+1):
+    #     print(i)
 
-    temp = series[i-1] + series[i-2]
-    
-    series.append(temp)
+        temp = series[i-1] + series[i-2]
 
-series.pop(0) # removing first element - zero
-series
+        series.append(temp)
 
-if 
+    series.pop(0) # removing first element - zero
+    print(series)
+    return
+
+if __name__ == '__main__':
+    main()
