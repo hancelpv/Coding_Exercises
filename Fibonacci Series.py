6596b33a195fba9903472f3d101623d6d940dcb5
Hancel PV <34185789+hancelpv@users.noreply.github.com>
2019-02-09 00:03:54 +0530

    Code Cleanup

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -1,21 +1,8 @@
 
-# coding: utf-8
-
-# In[6]:
-
-
 series = [0, 1]
-
-
-# In[7]:
-
 
 # Taking User input 
 num_points = int(input("Enter num of terms required in Fibonacci Sequence :"))
-
-
-# In[8]:
-
 
 for i in range(2, num_points+1):
 #     print(i)
@@ -24,10 +11,7 @@ for i in range(2, num_points+1):
     
     series.append(temp)
 
-
-# In[9]:
-
-
 series.pop(0) # removing first element - zero
 series
 
+if 
